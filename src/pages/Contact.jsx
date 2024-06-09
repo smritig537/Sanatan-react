2bb8d3fc6b8997f2fb3c37029f0ab611724ecf54
smriti gupta <smritig537@gmail.com>
2024-06-10 00:25:48 +0530

    adding

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,37 @@
-import WIP from "../components/WIP";
+import React from 'react'
 
 function Contact() {
-  return <WIP />;
+  return <>
+  <div className="flex flex-col items-center justify-between h-auto mt-10 mb-8 lg:flex-row ">
+    <div className="h-[600px] w-auto mx-4  bg-[#f08619] lg:w-[600px] lg:mx-0">
+ <h1 className="pt-20 text-3xl font-bold text-center text-white">Get in Touch</h1>
+ <h1 className="m-auto mt-8 text-center  font-semibold drop-shadow-lg  w-[50%] ">601, Kailash Building, K.G. Marg <br>New Delhi-110001</br></h1>
+ <h1 className="m-auto mt-8 text-center underline cursor-pointer" >sanatanadharmafoundation1@gmail.com</h1>
+    </div>
+    <form className="flex w-[50%] h-auto  flex-wrap justify-center items-start">
+       <div className="w-[90%] w-[70%] h-[50px] lg:mr-[20px] mt-[40px] lg:w-[35%]">
+        <p className="font-light text-grey ">First Name</p>
+        <input type="text" style={{margin:"20px;"}} placeholder="Enter First Name" className=" pl-4 w-[100%] rounded-[5px]   h-[40px] border-[1px] border-[grey] mr-12" />
+       </div>
+       <div className="w-[90%] w-[70%] h-[50px] mt-[40px] lg:w-[35%]">
+        <p className="font-light text-grey">Last Name</p>
+        <input type="text" placeholder="Enter Last Name" className=" pl-4 w-[100%] h-[40px] border-[1px] rounded-[5px] border-[grey]  " />
+       </div>
+       <div className="w-[90%] h-[50px] mt-[40px] lg:w-[70%]">
+        <p className="font-light text-grey">Email</p>
+        <input type="text" placeholder="Your Email" className="w-[100%] pl-4 h-[40px] border-[1px] rounded-[5px] border-[grey] " />
+       </div>
+       <div className="w-[90%] h-[50px] mt-[40px] lg:w-[70%]">
+        <p className="font-light text-grey">Message</p>
+        <input type="text" className="w-[100%]  h-[75px] border-[1px] rounded-[5px] border-[grey] " />
+       </div>
+       <button  className="bg-[#f08619] mt-[100px] text-white w-[90%] h-[40px] lg:w-[70%] shadow-slate-500">
+          Send
+       </button>
+    </form>
+  </div>
+  </>;
 }
 
 export default Contact;
+
