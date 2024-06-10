e7547f4ec25075c2a23cd84dbeb1dcdcf3b5ccb8
smriti gupta <smritig537@gmail.com>
2024-06-10 12:53:03 +0530

    last

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,13 @@ function Contact() {
       <div className="flex flex-col items-center justify-between h-auto mt-10 mb-8 lg:flex-row">
         <div className="h-[600px] w-auto mx-4 bg-[#f08619] lg:w-[600px] lg:mx-0">
           <h1 className="pt-20 text-3xl font-bold text-center text-white">Get in Touch</h1>
-          <h1 className="m-auto mt-8 text-center font-semibold drop-shadow-lg w-[50%]">601, Kailash Building, K.G. Marg <br />New Delhi-110001</h1>
-          <h1 className="m-auto mt-8 text-center underline cursor-pointer">sanatanadharmafoundation1@gmail.com</h1>
+          <h1 className="m-auto mt-8 text-center font-semibold drop-shadow-lg w-[50%] text-white">601, Kailash Building, K.G. Marg <br />New Delhi-110001</h1>
+          <h1 className="m-auto mt-8 text-center text-white underline cursor-pointer">sanatanadharmafoundation1@gmail.com</h1>
         </div>
-        <form className="flex w-[50%] h-auto flex-wrap justify-center items-start">
+        <form className="flex w-[50%] h-auto flex-col justify-center items-start">
         <div className="w-[90%] w-[70%] h-[50px] lg:mr-[20px] mt-[40px] lg:w-[35%]">
         <p className="font-light text-gray-500">First Name</p>
-        <input type="text" style={{ margin: "20px" }} placeholder="Enter First Name" className="pl-4 w-[100%] rounded-[5px] h-[40px] border-[1px] border-gray-500 mr-12" />
+        <input type="text" placeholder="Enter First Name" className="pl-4 w-[100%] rounded-[5px] h-[40px] border-[1px] border-gray-500 mr-12"  />
       </div>
       <div className="w-[90%] w-[70%] h-[50px] mt-[40px] lg:w-[35%]">
         <p className="font-light text-gray-500">Last Name</p>
