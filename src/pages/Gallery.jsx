8159adf4068b968d561ba16acd30e2d758d187ee
smriti gupta <smritig537@gmail.com>
2024-07-23 12:18:25 +0530

    kk

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -5,7 +5,7 @@ const imageUrl = {
   "Social Welfare": [1, 2, 3, 4, 5, 6, 7],
   "Medical support": [1, 2, 3, 4],
   Awards: [1, 2, 3, 4],
-  Political: [2, 3, 4, 5, 6, 7, 8],
+  Political: [2, 3, 4, 5, 6, 7, 8,9],
   "Women's Welfare": [1, 3, 4, 5, 6, 2],
   "Spiritual":[1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18,19,20,21,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58]
 };
@@ -19,7 +19,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Social Welfare
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Social Welfare"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/SocialWelfare/${e}.jpg`}
@@ -33,7 +33,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Political
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Political"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/Political/${e}.jpg`}
@@ -47,7 +47,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Awards
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Awards"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/Awards/${e}.jpg`}
@@ -62,7 +62,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Women's Welfare
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Women's Welfare"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/Womens Welfare/${e}.jpg`}
@@ -77,7 +77,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Medical Support
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Medical support"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/Medical Support/${e}.jpg`}
@@ -90,7 +90,7 @@ function Gallery() {
       <h1 className="font-oswald text-[#313131] mt-5 font-bold text-3xl text-center">
         Spiritual Events
       </h1>
-      <div className="grid w-4/5 grid-cols-3 gap-4 mx-auto my-5">
+      <div className="grid w-4/5 grid-cols-2 gap-4 mx-auto my-5">
         {imageUrl["Spiritual"].map((e) => (
           <img
             src={`${process.env.PUBLIC_URL}/gallery/Spiritual/${e}.jpg`}
