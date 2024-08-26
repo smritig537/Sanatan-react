c0f6dae75f5cc65bc87f492fbf33f44b7e03dd5b
sadre <mrsadre20@gmail.com>
2024-08-26 14:37:17 +0530

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,10 @@ import { default as Logo } from "../assets/LOGO.jpg";
 
 function Footer() {
 return (
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer class="bg-[#F08619] rounded-lg shadow dark:bg-gray-900 m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src={Logo} class="h-14" alt="Sanatan Dharma Foundation Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sanatan Dharma Foundation</span>
-            </a>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Sanatan Dharma Foundation</span>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" class="hover:underline hover:text-[#F08619] me-4 md:me-6">About</a>
