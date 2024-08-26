237079f93bbd82507777fe2ed450d4cfe3d327da
sadre <mrsadre20@gmail.com>
2024-08-26 14:44:49 +0530

    compelete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,16 +12,16 @@ return (
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Sanatan Dharma Foundation</span>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">About</a>
+                    <a href="#" class="hover:underline text-black me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">Supporters</a>
+                    <a href="#" class="hover:underline text-black me-4 md:me-6">Supporters</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">Media</a>
+                    <a href="#" class="hover:underline text-black me-4 md:me-6">Media</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-black">Gallery</a>
+                    <a href="#" class="hover:underline text-black">Gallery</a>
                 </li>
             </ul>
         </div>
