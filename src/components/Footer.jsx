1258a624d22bb5f16829048599fae10a067a23f0
sadre <mrsadre20@gmail.com>
2024-08-26 14:41:17 +0530

    footer correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,26 +6,26 @@ import { default as Logo } from "../assets/LOGO.jpg";
 
 function Footer() {
 return (
-<footer class="bg-[#F08619] rounded-lg shadow dark:bg-gray-900 m-4">
+<footer class="bg-[#F08619] rounded-lg shadow dark:bg-gray-900 w-full">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Sanatan Dharma Foundation</span>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline hover:text-[#F08619] me-4 md:me-6">About</a>
+                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-[#F08619] me-4 md:me-6">Supporters</a>
+                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">Supporters</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-[#F08619] me-4 md:me-6">Media</a>
+                    <a href="#" class="hover:underline hover:text-black me-4 md:me-6">Media</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline hover:text-[#F08619]">Gallery</a>
+                    <a href="#" class="hover:underline hover:text-black">Gallery</a>
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-[#F08619] sm:mx-auto dark:border-[#F08619] lg:my-8" />
+        <hr class="my-6 border-black sm:mx-auto dark:border-black lg:my-8" />
         <div className="flex justify-center pb-3 gap-1">
      
         <a
@@ -44,7 +44,7 @@ return (
           <img className="h-7 rounded-md" src={InstagramIcon} alt="" />
         </a>
       </div>
-        <span class="block text-sm text-[#F08619] sm:text-center dark:text-gray-400">© 2024 <a href="https://sanatanadharmafoundation.co/" class="hover:underline">Sanatan Dharma Foundation</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-white sm:text-center dark:text-gray-400">© 2024 <a href="https://sanatanadharmafoundation.co/" class="hover:underline">Sanatan Dharma Foundation</a>. All Rights Reserved.</span>
     </div>
 </footer>
 );
