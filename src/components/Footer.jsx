271b304bb8767e72d51dc3db2ea3f94ba8b2b78a
sadre <mrsadre20@gmail.com>
2024-08-26 14:24:50 +0530

    footer logo add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ return (
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="public/logo.png" class="h-14" alt="Sanatan Dharma Foundation Logo" />
+                <img src="../assets/logo.png" class="h-14" alt="Sanatan Dharma Foundation Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sanatan Dharma Foundation</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
