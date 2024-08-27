669d119b491795867d0c4e248ae814df9d49505d
sadre <mrsadre20@gmail.com>
2024-08-27 11:55:12 +0530

    tags change on swami ji about

--- src/components/SwamijiAbout.jsx
+++ src/components/SwamijiAbout.jsx
@@ -1,10 +1,10 @@
 function SwamijiAbout({ title, content }) {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-[20%_80%] gap-x-5">
-        <div className="text-4xl mb-2 text-center text-orange-900">{title}</div>
-        <div className="text-gray-700">{content}</div>
-      </div>
+      <main className="grid grid-cols-1 sm:grid-cols-[20%_80%] gap-x-5">
+        <h2 className="text-4xl mb-2 text-center text-orange-900">{title}</h2>
+        <p className="text-gray-700">{content}</p>
+      </main>
     </>
   );
 }
