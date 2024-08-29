ca826358b20d59e0b1cd93746e05a3ea677f0066
sadre <mrsadre20@gmail.com>
2024-08-29 12:13:02 +0530

    Home page heading and paragraph structure change

--- src/components/SwamijiAbout.jsx
+++ src/components/SwamijiAbout.jsx
@@ -1,7 +1,7 @@
 function SwamijiAbout({ title, content }) {
   return (
     <>
-      <main className="grid grid-cols-1 sm:grid-cols-[20%_80%] gap-x-5">
+      <main className="grid grid-cols-1 gap-x-5">
         <h2 className="text-4xl mb-2 text-center text-orange-900">{title}</h2>
         <p className="text-gray-700">{content}</p>
       </main>
