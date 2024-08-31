721756df6cd42e21b62cb29718aa8f01395e54f9
sadre <mrsadre20@gmail.com>
2024-08-31 14:35:14 +0530

    Faq quetions add

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -3,33 +3,33 @@ import React, { useState } from 'react';
 const faqData = [
   {
     id: 1,
-    question: "When will my order arrive?",
-    answer: "Shipping time is set by our delivery partners, according to the delivery method chosen by you. Additional details can be found in the order confirmation."
+    question: "What is the Sanatana Dharma Foundation?",
+    answer: "The Sanatana Dharma Foundation is an organization dedicated to helping people explore and deepen their understanding of Hinduism, also known as Sanatana Dharma. We offer a range of programs such as spiritual workshops, cultural festivals, and meditation retreats. Our goal is to guide individuals on their spiritual journey, promote Vedic traditions, and foster a deeper connection with Hindu philosophy. By participating in our events and initiatives, you can reconnect with ancient wisdom, engage with a supportive community, and enrich your spiritual life"
   },
   {
     id: 2,
-    question: "How do I track my order?",
-    answer: "Once shipped, you’ll get a confirmation email that includes a tracking number and additional information regarding tracking your order."
+    question: "What kind of programs does the Sanatana Dharma Foundation offer?",
+    answer: "The foundation offers a range of programs including spiritual workshops, cultural festivals, and meditation retreats. These programs are designed to help people understand Hindu philosophy, practice Vedic traditions, and connect with a supportive community."
   },
   {
     id: 3,
-    question: "What’s your return policy?",
-    answer: "We allow the return of all items within 30 days of your original order’s date. If you’re interested in returning your items, send us an email with your order number and we’ll ship a return label."
+    question: "What is the mission of the Sanatana Dharma Foundation?",
+    answer: "The mission of the Sanatana Dharma Foundation is to provide a space for individuals to explore their spirituality, deepen their understanding of Hindu teachings, and connect with others. They focus on spreading messages of peace, love, and unity through their various outreach and cultural events."
   },
   {
     id: 4,
-    question: "How do I make changes to an existing order?",
-    answer: "Changes to an existing order can be made as long as the order is still in “processing” status. Please contact our team via email and we’ll make sure to apply the needed changes. If your order has already been shipped, we cannot apply any changes to it. If you are unhappy with your order when it arrives, please contact us for any changes you may require."
+    question: "Who is the founder of the Sanatana Dharma Foundation?",
+    answer: "The founder of the Sanatana Dharma Foundation is Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit Swami Narayan Nand Giriji Maharaj. He is a respected spiritual leader dedicated to promoting and preserving the values and teachings of Sanatana Dharma."
   },
   {
     id: 5,
-    question: "What shipping options do you have?",
-    answer: "For USA domestic orders we offer FedEx and USPS shipping."
+    question: "What is Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit Swami Narayan Nand Giriji Maharaj's background?",
+    answer: "Before becoming a spiritual leader, Mahamadaleshwar Giriji Maharaj had a successful career as a businessman. He later transitioned to spiritual leadership and has been actively involved in education and social activism, working to make a positive impact on society."
   },
   {
     id: 6,
-    question: "What payment methods do you accept?",
-    answer: "Any method of payments acceptable by you. For example: We accept MasterCard, Visa, American Express, PayPal, JCB Discover, Gift Cards, etc."
+    question: "How is the Sanatana Dharma Foundation working to preserve Sanatan culture?",
+    answer: "The foundation, under the leadership of Mahamadaleshwar Giriji Maharaj, is working to promote Sanatan Dharma by spreading awareness and cultural pride, especially among the youth. They send books to temples across India and aim to foster a deeper understanding and appreciation of Sanatan culture."
   }
 ];
 
@@ -65,7 +65,7 @@ const FAQ = () => {
     <main className="p-5 bg-light-blue">
       <div className="flex justify-center items-start my-2">
         <div className="w-full sm:w-10/12 md:w-1/2 my-1">
-          <h2 className="text-xl font-semibold text-vnet-blue mb-2">FAQ - Order, Shipping, Etc.</h2>
+          <h2 className="text-xl font-semibold text-vnet-blue mb-2">FAQ - Sanatana Dharma Foundation</h2>
           <ul className="flex flex-col">
             {faqData.map((faq) => (
               <FAQItem
