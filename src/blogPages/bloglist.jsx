7dc6dbd4af7192f7813cbebf8796bc891e1eaa47
sadre <mrsadre20@gmail.com>
2024-09-06 15:19:52 +0530

    tags add

--- src/blogPages/bloglist.jsx
+++ src/blogPages/bloglist.jsx
@@ -5,21 +5,24 @@ function Bloglist() {
     {
       id: 1,
       name: "Sanatana Dharma Foundation: Reviving Ancient Wisdom and Modern Values",
-      Link: "/blogs/blog1",
+      link: "/blogs/blog1",
     },
   ];
 
   return (
-    <div className="w-full md:w-1/4 px-4">
+    <section className="w-full md:w-1/4 px-4">
       <div className="bg-gray-100 p-4">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">Recent Posts</h2>
+        <h2 className="text-2xl font-bold text-gray-800 mb-4" aria-level="2">
+          Recent Posts
+        </h2>
         <ol className="list-decimal pl-5 space-y-2">
           {Lists.map((list) => (
             <li className="text-lg" key={list.id}>
               <a
-                href={list.Link}
+                href={list.link}
                 className="text-blue-700 hover:underline break-words"
                 style={{ whiteSpace: "normal" }}
+                aria-label={`Read more about ${list.name}`}
               >
                 {list.name}
               </a>
@@ -27,7 +30,7 @@ function Bloglist() {
           ))}
         </ol>
       </div>
-    </div>
+    </section>
   );
 }
 
