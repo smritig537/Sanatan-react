7c7cd3979abc95d819240bc3904166e5594a3c18
sadre <mrsadre20@gmail.com>
2024-09-07 16:57:09 +0530

    home page seo done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
 import ImageSlider from "../components/ImageSlider";
 import Swamiji from "./Swami ji1.jpg";
 import SwamijiAbout from "../components/SwamijiAbout";
@@ -9,28 +9,35 @@ import swamiji1 from "../assets/roll1.webp";
 import swamiji2 from "../assets/roll2.webp";
 import swamiji3 from "../assets/roll3.webp";
 import guru from "../assets/guru.jpg";
+import { Helmet } from "react-helmet-async";
+
 const Home = () => {
   return (
     <main>
+      <Helmet>
+        <title>Sanatana Dharma Foundation - Reviving Ancient culture</title>
+        <meta
+          name="description"
+          content="The Sanatana Dharma Foundation offers spiritual workshops, cultural festivals, and meditation retreats. Explore Sanatana Dharma (Hinduism) and join a vibrant community dedicated to preserving Vedic traditions."
+        />
+        <meta
+          name="keywords"
+          content="Sanatana Dharma Foundation, Hinduism, spiritual workshops, meditation retreats, cultural festivals, Vedic traditions, Sanatana Dharma"
+        />
+      </Helmet>
+
       <div className="flex-grow">
         <ImageSlider />
         <div className="m-auto sm:w-[986px] text-xl mb-10 p-5 sm:p-0">
           <h1 className="font-oswald text-[#313131] font-bold text-5xl text-center my-10">
-          Sanatana Dharma Foundation
+            Sanatana Dharma Foundation
           </h1>
           <p className="items-center mb-3 text-justify rounded-sm">
-            The Sanatana Dharma Foundation is dedicated to helping individuals
-            reconnect with the{" "}
+            The Sanatana Dharma Foundation is dedicated to helping individuals reconnect with the{" "}
             <Link to="sanatanadharma" className="text-blue-600 underline">
               teachings of Sanatana Dharma
             </Link>{" "}
-            (Hinduism). It offers a range of programs, including spiritual
-            workshops, cultural festivals, and meditation retreats, designed to
-            provide guidance and support on one's spiritual journey. By
-            preserving Vedic traditions and promoting Hindu philosophy, the
-            foundation creates a vibrant community space for learning, growth,
-            and connection, fostering a deeper understanding of ancient wisdom
-            and values.
+            (Hinduism). It offers a range of programs, including spiritual workshops, cultural festivals, and meditation retreats, designed to provide guidance and support on one's spiritual journey. By preserving Vedic traditions and promoting Hindu philosophy, the foundation creates a vibrant community space for learning, growth, and connection, fostering a deeper understanding of ancient wisdom and values.
           </p>
           <div className="flex flex-col gap-5 mx-[20px] mb-10 text-justify">
             <SwamijiAbout
@@ -41,17 +48,15 @@ const Home = () => {
 
           <div className="flex flex-col gap-5 mx-[20px] mb-10 text-justify">
             <SwamijiAbout
-              title="Core of Sanatana Dharma:"
-              content="At the core of Sanatana Dharma are the timeless sanskrit teachings that have been passed down through the ages. These teachings encompass a wide range of concepts, from the concept of karma – the law of cause and effect – to the importance of upholding moral values and ethical principles in our daily lives. Through spiritual enlightenment, yoga workshops, and interfaith initiatives, the foundation aims to provide individuals with the guidance and support they need to navigate their spiritual journey.
-            \nOne of the key aspects of Sanatana Dharma is the emphasis on community and connection. Through a variety of programs and events, such as religious education classes, cultural festivals, and meditation retreats, the foundation creates a space for individuals to come together and learn from one another. By preserving vedic traditions and promoting Hindu philosophy, the foundation seeks to uphold traditional values and ensure that the spiritual heritage of Sanatana Dharma continues to thrive."
+              title="Core of Sanatana Dharma"
+              content="At the core of Sanatana Dharma are the timeless Sanskrit teachings that have been passed down through the ages. These teachings encompass a wide range of concepts, from the concept of karma – the law of cause and effect – to the importance of upholding moral values and ethical principles in our daily lives. Through spiritual enlightenment, yoga workshops, and interfaith initiatives, the foundation aims to provide individuals with the guidance and support they need to navigate their spiritual journey. One of the key aspects of Sanatana Dharma is the emphasis on community and connection. Through a variety of programs and events, such as religious education classes, cultural festivals, and meditation retreats, the foundation creates a space for individuals to come together and learn from one another. By preserving Vedic traditions and promoting Hindu philosophy, the foundation seeks to uphold traditional values and ensure that the spiritual heritage of Sanatana Dharma continues to thrive."
             />
           </div>
 
           <div className="flex flex-col gap-5 mx-[20px] mb-10 text-justify">
             <SwamijiAbout
-              title="Mission of Sanatana Dharma Foundation:"
-              content="Whether you are looking for spiritual guidance, philosophical teachings, or simply a community of like-minded individuals, the Sanatana Dharma Foundation offers a space for individuals to explore their spirituality and deepen their connection to the sacred texts and teachings of Hinduism. Through outreach programs and cultural events, the foundation is dedicated to spreading the message of peace, love, and unity to all who seek it.
-            /nSo, if you are looking to reconnect with your spiritual roots, deepen your understanding of ancient wisdom, and engage with a vibrant community of spiritual seekers, consider getting involved with the Sanatana Dharma Foundation. Your spiritual journey awaits!"
+              title="Mission of Sanatana Dharma Foundation"
+              content="Whether you are looking for spiritual guidance, philosophical teachings, or simply a community of like-minded individuals, the Sanatana Dharma Foundation offers a space for individuals to explore their spirituality and deepen their connection to the sacred texts and teachings of Hinduism. Through outreach programs and cultural events, the foundation is dedicated to spreading the message of peace, love, and unity to all who seek it. So, if you are looking to reconnect with your spiritual roots, deepen your understanding of ancient wisdom, and engage with a vibrant community of spiritual seekers, consider getting involved with the Sanatana Dharma Foundation. Your spiritual journey awaits!"
             />
           </div>
 
@@ -61,16 +66,12 @@ const Home = () => {
             alt="Sanatana Dharma Foundation owner"
           />
           <p className="items-center mb-5 text-justify rounded-sm bg-orange-50">
-            Our founder, Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit
-            Swami Narayan Nand Giriji Maharaj, is an esteemed spiritual leader
-            and devoted activist of Sanatan Dharma. He is a proud citizen of
-            Bharat (India) and actively promotes the ancient traditions, values,
-            and teachings of Sanatan Dharma.
+            Our founder, Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit Swami Narayan Nand Giriji Maharaj, is an esteemed spiritual leader and devoted activist of Sanatan Dharma. He is a proud citizen of Bharat (India) and actively promotes the ancient traditions, values, and teachings of Sanatan Dharma.
           </p>
 
           <div className="flex flex-col gap-5 mx-[20px] mb-10 text-justify">
             <SwamijiAbout
-              title="Earlier life"
+              title="Earlier Life"
               content="Mahamadaleshwar Giriji Maharaj pursued multiple careers before dedicating himself to spiritual leadership. He began as a successful businessman, contributing to the economic landscape. However, driven by his spiritual calling, he transitioned to become a renowned spiritual leader. Additionally, he has been involved in the fields of education and social activism, striving to create a positive impact on society."
             />
 
@@ -98,29 +99,28 @@ const Home = () => {
               <img
                 className="object-cover object-center m-auto rounded-full h-72 w-72 "
                 src={swamiji1}
-                alt="Swami jii in kashi"
+                alt="Swami ji in Kashi"
               />
               <img
                 className="object-cover object-center m-auto rounded-full h-72 w-72 "
                 src={swamiji2}
-                alt="Swami jii in kashi"
+                alt="Swami ji in Kashi"
               />
               <img
                 className="object-cover object-center m-auto rounded-full h-72 w-72 "
                 src={guru}
-                alt="Swami jii in kashi"
+                alt="Swami ji"
               />
             </div>
-            <h1 className="p-1 mt-5 mb-5 text-center">
-              "The Sanatana Dharma Foundation is dedicated to the revival and
-              resurrection of the ancient Sanatana Dharma and Ideologies."
-            </h1>
+            <h2 className="p-1 mt-5 mb-5 text-center">
+              "The Sanatana Dharma Foundation is dedicated to the revival and resurrection of the ancient Sanatana Dharma and Ideologies."
+            </h2>
 
             <Divider />
 
-            <h1 className="font-oswald text-[#313131] font-bold text-5xl text-center my-10">
+            <h2 className="font-oswald text-[#313131] font-bold text-5xl text-center my-10">
               Interest Form
-            </h1>
+            </h2>
 
             <Form subTitle="I am interested to associate / support the Sanatana Dharma Foundation. My details are below:" />
           </div>
