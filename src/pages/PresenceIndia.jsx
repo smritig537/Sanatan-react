626a1f74d18b5f0467a9e0a03dee642fdf53cf9d
sadre <mrsadre20@gmail.com>
2024-09-09 14:18:57 +0530

    presennce in india

--- src/pages/PresenceIndia.jsx
+++ src/pages/PresenceIndia.jsx
@@ -1,171 +1,133 @@
 import Divider from "../components/Divider";
 import React from "react";
 import India from "../assets/india.png";
-
+import { Helmet } from "react-helmet-async";
 
 function PresenceIndia() {
   return (
-    
-    <main className="flex-grow w-4/5 m-auto ">
-      <h1 className="font-oswald text-[#313131] font-bold text-5xl text-center mt-10">
-        Locations in India
-      </h1>
+    <main className="flex-grow w-4/5 m-auto">
+      <Helmet>
+        <title>Sanatana Dharma Foundation - Locations and Heritage in India</title>
+        <meta name="description" content="Explore the presence of Sanatana Dharma Foundation across major Indian cities, its commitment to reviving ancient culture, and the rich heritage of Hinduism." />
+        <meta name="keywords" content="Sanatana Dharma, Hinduism, Indian Culture, Indian Cities, Hindu Pilgrimage, Indian Traditions" />
+      </Helmet>
+
+      <header>
+        <h1 className="font-oswald text-[#313131] font-bold text-5xl text-center mt-10">
+          Locations in India
+        </h1>
+      </header>
+
       <Divider />
-      <p className="text-gray-600 font-semibold text-2xl text-center">
-        Delhi | Mumbai | Hyderabad | Indore | Chennai
-      </p>
-      <img src={India} alt="" className="mx-auto w-2/3 mt-20" />
 
-      <div>
+      <section>
+        <p className="text-gray-600 font-semibold text-2xl text-center">
+          Delhi | Mumbai | Hyderabad | Indore | Chennai
+        </p>
+        <img src={India} alt="Map showing the presence of Sanatana Dharma Foundation in India" className="mx-auto w-2/3 mt-20" />
+      </section>
+
+      <section>
         <h2 className="font-oswald text-[#313131] font-bold text-3xl text-center my-8">
-          Sanatana Dharma Foundation: Reviving Ancient Indian Culture and
-          Traditions
+          Sanatana Dharma Foundation: Reviving Ancient Indian Culture and Traditions
         </h2>
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center  my-4">
-          Our Presence Across Major Indian Cities
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          The <b>Sanatana Dharma Foundation</b> has a significant presence
-          across major Indian cities, with its head office in <b>Delhi</b> and
-          branches in <b>Chennai, Mumbai, Hyderabad,</b> and <b>Indore</b>.
-          These strategic locations enable us to effectively carry out our
-          mission of the social reincarnation of
-          <b>Sanatana Dharma</b>, providing religious and ethical training
-          tailored to contemporary needs.
-        </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Promoting a Unified Approach to Sanatana Thought
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Our foundation is committed to fostering a wide, liberal, and
-          unsectarian approach to <b>Sanatana Dharma</b>, uniting diverse forms
-          under a common ethos. In each city, we actively engage with local
-          communities and like-minded individuals who value their national
-          identity and pride in being followers of <b>Sanatana Dharma</b>.
-        </p>
+        <article>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Our Presence Across Major Indian Cities
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            The <strong>Sanatana Dharma Foundation</strong> has a significant presence across major Indian cities, with its head office in <strong>Delhi</strong> and branches in <strong>Chennai, Mumbai, Hyderabad,</strong> and <strong>Indore</strong>. These strategic locations enable us to effectively carry out our mission of the social reincarnation of <strong>Sanatana Dharma</strong>, providing religious and ethical training tailored to contemporary needs.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Addressing Cultural Challenges and Reviving Ancient Dharma
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          By addressing the challenges posed by western influences that have
-          marginalized <b>Indian culture</b>, we strive to revive and uphold the
-          ancient <b>Dharma</b>, promoting righteousness, integrity, and mutual trust.
-          Our efforts in these cities focus on excluding divisive elements and
-          reinforcing a common faith that transcends religious and cultural
-          biases, thereby restoring the cultural wealth and unity of our nation.
-        </p>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Promoting a Unified Approach to Sanatana Thought
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Our foundation is committed to fostering a wide, liberal, and unsectarian approach to <strong>Sanatana Dharma</strong>, uniting diverse forms under a common ethos. In each city, we actively engage with local communities and like-minded individuals who value their national identity and pride in being followers of <strong>Sanatana Dharma</strong>.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Uniting Indians in a Shared Heritage
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Through our widespread presence, we aim to unite Indians in a shared
-          heritage, reviving <b>Sanatana Dharma</b> not just as a religion, but as an
-          ancient school of beliefs that serves the needs and rights of all
-          citizens of <b>Bharat</b>. Our foundation’s initiatives are dedicated to
-          promoting the principles of <b>Sanatana Dharma</b>, ensuring that the rich
-          traditions and values of <b>Indian culture</b> are preserved and celebrated.
-        </p>
-      </div>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Addressing Cultural Challenges and Reviving Ancient Dharma
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            By addressing the challenges posed by western influences that have marginalized <strong>Indian culture</strong>, we strive to revive and uphold the ancient <strong>Dharma</strong>, promoting righteousness, integrity, and mutual trust. Our efforts in these cities focus on excluding divisive elements and reinforcing a common faith that transcends religious and cultural biases, thereby restoring the cultural wealth and unity of our nation.
+          </p>
 
-      <div>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Uniting Indians in a Shared Heritage
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Through our widespread presence, we aim to unite Indians in a shared heritage, reviving <strong>Sanatana Dharma</strong> not just as a religion, but as an ancient school of beliefs that serves the needs and rights of all citizens of <strong>Bharat</strong>. Our foundation’s initiatives are dedicated to promoting the principles of <strong>Sanatana Dharma</strong>, ensuring that the rich traditions and values of <strong>Indian culture</strong> are preserved and celebrated.
+          </p>
+        </article>
+      </section>
+
+      <section>
         <h2 className="font-oswald text-[#313131] font-bold text-3xl text-center my-8">
           Discover the Rich Heritage of Hinduism and Indian Culture
         </h2>
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center  my-4">
-          Introduction to Hinduism
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Hinduism, one of the world’s oldest religions, is deeply rooted in the
-          Indian subcontinent. It encompasses a wide range of beliefs and
-          practices, making it a unique and diverse faith. Central to Hinduism
-          are the concepts of <b>karma</b> and <b>dharma</b>, which guide the moral and
-          ethical conduct of its followers.
-        </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          The Essence of Karma and Dharma
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-        <b>Karma</b> refers to the law of cause and effect, where every action has a
-          corresponding reaction. This belief encourages individuals to lead a
-          righteous life, as good deeds lead to positive outcomes. <b>Dharma</b>
-          represents duty, righteousness, and moral order, emphasizing the
-          importance of living in harmony with the universe.
-        </p>
+        <article>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Introduction to Hinduism
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Hinduism, one of the world’s oldest religions, is deeply rooted in the Indian subcontinent. It encompasses a wide range of beliefs and practices, making it a unique and diverse faith. Central to Hinduism are the concepts of <strong>karma</strong> and <strong>dharma</strong>, which guide the moral and ethical conduct of its followers.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Ancient Scriptures and Texts
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Hinduism boasts a rich collection of sacred texts, including the
-          <b>Vedas, Upanishads, Bhagavad Gita,</b> and <b>Puranas</b>. These scriptures
-          provide profound insights into the philosophy, rituals, and spiritual
-          practices of Hinduism. The <b>Vedas</b>, considered the oldest scriptures,
-          lay the foundation for Hindu beliefs and rituals.
-        </p>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            The Essence of Karma and Dharma
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            <strong>Karma</strong> refers to the law of cause and effect, where every action has a corresponding reaction. This belief encourages individuals to lead a righteous life, as good deeds lead to positive outcomes. <strong>Dharma</strong> represents duty, righteousness, and moral order, emphasizing the importance of living in harmony with the universe.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          The Pantheon of Hindu Gods and Deities
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Hinduism is known for its vast array of gods and deities, each
-          representing different aspects of life and the universe. Prominent
-          deities include <b>Brahma</b>, the creator; <b>Vishnu</b>, the preserver; and <b>Shiva</b>,
-          the destroyer. Devotees engage in various forms of worship and rituals
-          to honor these deities.
-        </p>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Ancient Scriptures and Texts
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Hinduism boasts a rich collection of sacred texts, including the <strong>Vedas, Upanishads, Bhagavad Gita,</strong> and <strong>Puranas</strong>. These scriptures provide profound insights into the philosophy, rituals, and spiritual practices of Hinduism. The <strong>Vedas</strong>, considered the oldest scriptures, lay the foundation for Hindu beliefs and rituals.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Yoga and Meditation in Hinduism
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-        <b>Yoga</b> and <b>meditation</b> are integral practices in Hinduism, aimed at
-          achieving spiritual enlightenment and inner peace. <b>Bhakti Yoga</b> focuses
-          on devotion and love for the divine, while <b>Jnana Yoga</b> emphasizes
-          knowledge and wisdom. These practices help individuals connect with
-          their inner selves and the divine.
-        </p>
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Indian Culture and Traditions
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Indian culture is a vibrant tapestry of traditions, customs, and
-          rituals. From colorful festivals to intricate art forms, Indian
-          culture reflects the diversity and richness of its heritage. <b>Sanatana
-          Dharma</b>, another term for Hinduism, signifies the eternal and universal
-          principles that underpin Indian culture.
-        </p>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            The Pantheon of Hindu Gods and Deities
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Hinduism is known for its vast array of gods and deities, each representing different aspects of life and the universe. Prominent deities include <strong>Brahma</strong>, the creator; <strong>Vishnu</strong>, the preserver; and <strong>Shiva</strong>, the destroyer. Devotees engage in various forms of worship and rituals to honor these deities.
+          </p>
 
-        <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
-          Pilgrimage Sites in India
-        </h3>
-        <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          India is home to numerous sacred sites and pilgrimage destinations,
-          such as <b>Varanasi, Rishikesh,</b> and <b>Tirupati</b>. These sites attract
-          millions of devotees seeking spiritual solace and blessings.
-          Pilgrimages are an essential aspect of Hindu religious practice,
-          fostering a sense of community and devotion.
-        </p>
-      </div>
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Yoga and Meditation in Hinduism
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            <strong>Yoga</strong> and <strong>meditation</strong> are integral practices in Hinduism, aimed at achieving spiritual enlightenment and inner peace. <strong>Bhakti Yoga</strong> focuses on devotion and love for the divine, while <strong>Jnana Yoga</strong> emphasizes knowledge and wisdom. These practices help individuals connect with their inner selves and the divine.
+          </p>
+
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Indian Culture and Traditions
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            Indian culture is a vibrant tapestry of traditions, customs, and rituals. From colorful festivals to intricate art forms, Indian culture reflects the diversity and richness of its heritage. <strong>Sanatana Dharma</strong>, another term for Hinduism, signifies the eternal and universal principles that underpin Indian culture.
+          </p>
+
+          <h3 className="font-oswald text-[#313131] font-bold text-2xl text-center my-4">
+            Pilgrimage Sites in India
+          </h3>
+          <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
+            India is home to numerous sacred sites and pilgrimage destinations, such as <strong>Varanasi, Rishikesh,</strong> and <strong>Tirupati</strong>. These sites attract millions of devotees seeking spiritual solace and blessings. Pilgrimages are an essential aspect of Hindu religious practice, fostering a sense of community and devotion.
+          </p>
+        </article>
+      </section>
 
       <summary>
         <h2 className="font-oswald text-[#313131] font-bold text-3xl text-center my-8">
           Conclusion
         </h2>
         <p className="items-center m-auto w-[90%] p-1 mt-2 mb-2 text-justify rounded-sm text-gray-700 text-xl">
-          Hinduism and Indian culture offer a profound and enriching experience,
-          rooted in ancient wisdom and timeless traditions. Whether through the
-          study of sacred texts, the practice of yoga and meditation, or
-          participation in cultural rituals, individuals can explore the depth
-          and beauty of this ancient religion. By understanding and embracing
-          the principles of <b>karma, dharma,</b> and the diverse practices of
-          Hinduism, one can lead a life of purpose, harmony, and spiritual
-          fulfillment.
+          Hinduism and Indian culture offer a profound and enriching experience, rooted in ancient wisdom and timeless traditions. Whether through the study of sacred texts, the practice of yoga and meditation, or participation in cultural rituals, individuals can explore the depth and beauty of this ancient religion. By understanding and embracing the principles of <strong>karma</strong>, <strong>dharma</strong>, and the diverse practices of Hinduism, one can lead a life of purpose, harmony, and spiritual fulfillment.
         </p>
-      </summary>
+        </summary>
     </main>
   );
 }
