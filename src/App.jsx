ea03c7e1faab5647a40e8bd9245f4eadb5186766
sadre <mrsadre20@gmail.com>
2024-09-09 15:50:20 +0530

    logo remove

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import Footer from "./components/Footer";
 import LogoComponent from "./components/LogoComponent";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
-import logo from "./assets/om-bg-remove.png"
+
 function App() {
   return (
     <div className="font-mukta flex flex-col min-h-screen">
-      <img src={logo} className="App-logo" alt="logo" />
       <LogoComponent />
       <Navbar />
       <Outlet />
