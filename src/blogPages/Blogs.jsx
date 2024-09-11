dfe401c3ea0da4206dc676163089287865b98e26
sadre <mrsadre20@gmail.com>
2024-09-11 17:46:40 +0530

    text center

--- src/blogPages/Blogs.jsx
+++ src/blogPages/Blogs.jsx
@@ -56,7 +56,7 @@ function Blogs() {
         <meta name="twitter:image" content={reviving} />
       </Helmet>
       <section className="px-8 mb-4">
-          <h1 className="text-4xl md:text-3xl pl-2 my-8 font-sans font-bold dark:text-gray-200">
+          <h1 className="text-4xl md:text-3xl text-center pl-2 my-8 font-sans font-bold dark:text-gray-200">
             Blogs
           </h1>
         
