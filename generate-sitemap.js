dea26674b17d288a9be2cac8e950f6559e6eb40b
sadre <mrsadre20@gmail.com>
2024-09-11 17:40:29 +0530

    sitemap ready

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const path = require('path');
 const { SitemapStream, streamToPromise } = require('sitemap');
@@ -5,13 +6,16 @@ const { createGzip } = require('zlib');
 
 // Create a list of URLs (you might fetch these dynamically from a database or API)
 const urls = [
-  { url: '/', changefreq: 'daily', priority: 1.0 },
-  { url: '/about', changefreq: 'monthly', priority: 0.8 },
-  { url: '/sanatanadharma', changefreq: 'monthly', priority: 0.8 },
-  { url: '/supporters', changefreq: 'monthly', priority: 0.8 },
-  { url: '/Locations-in-india', changefreq: 'monthly', priority: 0.8 },
-  { url: '/gallery', changefreq: 'monthly', priority: 0.8 },
- 
+{ url: '/', changefreq: 'daily', priority: 1.0 },
+{ url: '/about', changefreq: 'monthly', priority: 0.8 },
+{ url: '/sanatanadharma', changefreq: 'monthly', priority: 0.8 },
+{ url: '/supporters', changefreq: 'monthly', priority: 0.8 },
+{ url: '/Locations-in-india', changefreq: 'monthly', priority: 0.8 },
+{ url: '/gallery', changefreq: 'monthly', priority: 0.8 },
+{ url: '/blogs', changefreq: 'monthly', priority: 0.8 },
+{ url: '/blogs/reviving-ancient-wisdom', changefreq: 'monthly', priority: 0.8 },
+{ url: '/blogs/Hindu-Religious-Texts-and-Scriptures', changefreq: 'monthly', priority: 0.8 },
+
 ];
 
 // Create a write stream for sitemap.xml
@@ -27,5 +31,5 @@ sitemapStream.end();
 
 // Notify when the sitemap is created
 writeStream.on('finish', () => {
-  console.log('Sitemap created successfully!');
+console.log('Sitemap created successfully!');
 });
