68a6f79abbdd2400aaa01a49a5dda8cc9b010b66
sadre <mrsadre20@gmail.com>
2024-09-12 15:19:58 +0530

    social welfare

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Divider from "../components/Divider";
 
 const imageUrl = {
-  "Social Welfare": [1, 2, 3, 4, 5, 6, 7],
+  SocialWelfare: [1, 2, 3, 4, 5, 6, 7],
   "Medical support": [1, 2, 3, 4],
   Awards: [1, 2, 3, 4],
   Political: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
