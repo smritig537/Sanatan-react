101ce4a0da22f46a91a05a4cd57a17b470f5e40f
sadre <mrsadre20@gmail.com>
2024-10-11 14:17:57 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
   return (
     <nav className="bg-[#491c1d] border-[#f08619] py-2.5 dark:bg-gray-900">
       <div className="flex flex-wrap items-center justify-between w-full">
-        <a href="#" className="flex ml-3 items-center">
+        <a href="#" className="flex ml-4 items-center">
           <img src={logo} className="h-8 sm:h-12" alt="Sanatana Dharma Foundation logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+          <span className="self-center text-xl ml-1 font-semibold whitespace-nowrap text-white">
             Sanatana Dharma Foundation
           </span>
         </a>
