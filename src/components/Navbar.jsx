c44efc75d1daeb014644e7cc2ad3d2b534b7c139
sadre <mrsadre20@gmail.com>
2024-10-11 12:26:45 +0530

    old logo add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,94 +1,98 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import logo from "../assets/om-bg-remove.png";
+import { Link, useLocation, NavLink } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const location = useLocation();
+function Navbar() {
+const [isClicked, setIsClicked] = useState(false);
+const location = useLocation();
+useEffect(() => {
+setIsClicked();
+}, [location]);
+return (
+<nav
+className={`sm:w-11/12 w-screen sticky top-0 h-[60px] bg-[#F08619] m-auto ${
+isClicked ? "h-screen" : ""
+}`}
+>
+<div className="flex flex-row items-center justify-between mx-5 my-5 toggleButton sm:hidden">
+<h3 className="text-xl font-semibold text-white">Menu</h3>
+<button
+className="text-white bg-transparent border-none toggleButton sm:hidden"
+onClick={() => setIsClicked((prev) => !prev)}
+>
+<ReorderIcon color="inherit" />
+</button>
+</div>
+<div
+className={`${
+isClicked
+? "h-full flex flex-col items-center justify-start text-white "
+: "hidden sm:flex sm:flex-row sm:text-white sm:flex-grow sm:justify-center sm:items-center sm:h-full "
+}}`}
+>
+<NavLink id ="nav"
+to="."
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Home
+</NavLink>
+<NavLink id ="nav"
+to="about"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+About
+</NavLink>
+<NavLink id ="nav"
+to="sanatanadharma"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Sanatana Dharma
+</NavLink>
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+<NavLink id ="nav"
+to="Locations-in-india"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Locations in India
+</NavLink>
+<NavLink id ="nav"
+to="/gallery"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Gallery
+</NavLink>
 
-  useEffect(() => {
-    setIsOpen(false);
-  }, [location]);
+<NavLink id ="nav"
+to="/blogs"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Blogs
+</NavLink>
 
-  return (
-    <nav className="bg-[#491c1d] border-[#f08619] py-2.5 dark:bg-gray-900" role="navigation">
-      <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-        <div className="flex items-center">
-          <a href="/" className="flex items-center">
-            <img src={logo} className="h-8 mr-1 sm:h-12" alt="Sanatana Dharma Foundation Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
-              Sanatana Dharma Foundation
-            </span>
-          </a>
-        </div>
-        <div className="flex items-center lg:order-2">
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-white rounded-lg lg:hidden hover:bg-[#f084199a] focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu-2"
-            aria-expanded={isOpen}
-            aria-label={isOpen ? 'Close menu' : 'Open menu'}
-          >
-            <span className="sr-only">Open main menu</span>
-            {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div
-          className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${isOpen ? "block" : "hidden"}`}
-          id="mobile-menu-2"
-        >
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-            {["Home", "About", "Sanatana Dharma", "Locations in India", "Gallery", "Blogs", "Media", "Association & Support", "Supporters"].map((item, index) => (
-              <li key={index}>
-                <NavLink
-                  to={item.toLowerCase().replace(/ /g, "-")}
-                  className={({ isActive }) =>
-                    `block py-2 pl-3 pr-4 rounded ${
-                      isActive ? "bg-[#f08619] text-white" : "text-white hover:bg-[#f084199a]"
-                    } lg:bg-transparent lg:text-[#f08619] lg:p-0`
-                  }
-                  aria-current={item === "Home" ? "page" : undefined}
-                >
-                  {item}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-};
+<NavLink id ="nav"
+to="media"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Media
+</NavLink>
+
+<NavLink id ="nav"
+to="/contact"
+className={`no-underline text-white mx-4 sm:text-xs text-xl lg:text-xl`}
+>
+Association & Support
+</NavLink>
+
+<NavLink id ="nav"
+to="supporters"
+className={`no-underline text-white sm:text-xs text-xl mx-4 lg:text-xl`}
+>
+Supporters
+</NavLink>
+
+</div>
+</nav>
+);
+}
 
 export default Navbar;
