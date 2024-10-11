5f51c3d232d0104cabb20bc7302c433089fdddca
sadre <mrsadre20@gmail.com>
2024-10-11 12:09:09 +0530

    logo component remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Footer from "./components/Footer";
-import LogoComponent from "./components/LogoComponent";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <LogoComponent />
       <Navbar />
       <Outlet />
       <Footer />
