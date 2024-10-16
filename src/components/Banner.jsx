f55e8c43f14c2117058aa8a77be5d1141d236cb4
sadre <mrsadre20@gmail.com>
2024-10-16 12:53:13 +0530

    sanatan

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,122 +1,18 @@
-import banner from "./banner.webp";
-import sanatan from "./sanantan.jpg";
 
-import React, { useState, useEffect } from "react";
-import banner from "./car.jpg";
+import banner from "./banner.webp"
 
-const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = [
-    {
-      url: sanatan,
-      alt: "none",
-    },
-  ];
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + slides.length) % slides.length
+const Banner = () => {
+    return (
+        <>
+        
+        <img 
+        src={banner} 
+        alt="Sanatana Dharma Foundation Banner" 
+        loading='lazy'
+        className="aspect-auto w-full h-full"
+      />
+      </>
     );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
-    return () => clearInterval(interval); // Clear interval on component unmount
-  }, []);
-
-  return (
-    <div
-      id="default-carousel"
-      className="h-80 md:h-96 relative w-full"
-      data-carousel="slide"
-    >
-      <div className="relative h-80 md:h-96 overflow-hidden">
-        {" "}
-        {/* Increased height */}
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={`absolute w-full transition-opacity duration-700 ease-in-out ${
-              index === currentIndex ? "opacity-100" : "opacity-0"
-            }`} // Fade effect
-            data-carousel-item
-          >
-            <img
-              src={slide.url}
-              className="w-full h-full object-contain" // Changed to object-cover for better fitting
-              alt={slide.alt}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        {slides.map((_, index) => (
-          <button
-            key={index}
-            type="button"
-            className={`w-3 h-3 rounded-full ${
-              index === currentIndex ? "bg-blue-600" : "bg-gray-300"
-            }`}
-            aria-current={index === currentIndex}
-            aria-label={`Slide ${index + 1}`}
-            onClick={() => setCurrentIndex(index)}
-          />
-        ))}
-      </div>
-      <button
-        type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={prevSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={nextSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
-  );
 };
 
-export default Carousel;
+export default Banner;
