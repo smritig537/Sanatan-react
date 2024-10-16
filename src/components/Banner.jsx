de1c5cc37dc0fefef619449275edd82dc1b1f741
sadre <mrsadre20@gmail.com>
2024-10-16 14:56:06 +0530

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,13 +4,13 @@ import banner from "./banner.webp"
 const Banner = () => {
     return (
         <>
-        
+        <div className="w-full h-[400px]">
         <img 
         src={banner} 
         alt="Sanatana Dharma Foundation Banner" 
         loading='lazy'
-        className="aspect-auto w-full h-full"
-      />
+        className="aspect-auto w-full h-full object-contain"
+      /></div>
       </>
     );
 };
