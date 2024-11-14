792f9b25404bdb2581f009835d6611dfa890628a
sadre <mrsadre20@gmail.com>
2024-11-14 14:34:38 +0530

    icons add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,42 +44,42 @@ const Navbar = () => {
               </span>
             </Link>
             <div className="flex items-center lg:order-2">
-            <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-white rounded-lg lg:hidden hover:bg-[#f084199a] focus:outline-none"
-            aria-controls="mobile-menu-2"
-            aria-expanded={isOpen}
-          >
-            <span className="sr-only">Open main menu</span>
-            {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              <button
+                onClick={toggleMenu}
+                type="button"
+                className="inline-flex items-center p-2 ml-1 text-sm text-white rounded-lg lg:hidden hover:bg-[#f084199a] focus:outline-none"
+                aria-controls="mobile-menu-2"
+                aria-expanded={isOpen}
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
-          </button>
+                <span className="sr-only">Open main menu</span>
+                {isOpen ? (
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </button>
             </div>
             <div
               className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${isOpen ? "block" : "hidden"}`}
@@ -88,46 +88,52 @@ const Navbar = () => {
               <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                   <NavLink to="/" className={({ isActive }) =>
-                      `flex items-center py-2 pl-3 pr-4 rounded ${
-                        isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"
-                      } lg:bg-transparent lg:p-0`
+                      `flex items-center py-2 pl-3 pr-4 rounded ${isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"} lg:bg-transparent lg:p-0`
                     }>
-                    Home
+                    <i className="fas fa-home mr-2"></i> Home
                   </NavLink>
                 </li>
 
                 <li>
                   <NavLink to="/about" className={({ isActive }) => `block py-2 pl-3 pr-4 rounded ${isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"} lg:bg-transparent lg:p-0`}>
-                    About
+                    <i className="fas fa-info-circle mr-2"></i> About
                   </NavLink>
                 </li>
+
                 <li>
                   <NavLink to="/sanatanadharma" className={({ isActive }) => `block py-2 pl-3 pr-4 rounded ${isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"} lg:bg-transparent lg:p-0`}>
-                    Sanatana Dharma
+                    <i className="fas fa-book mr-2"></i> Sanatana Dharma
                   </NavLink>
                 </li>
+
                 <li>
                   <NavLink to="/locations-in-india" className={({ isActive }) => `block py-2 pl-3 pr-4 rounded ${isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"} lg:bg-transparent lg:p-0`}>
-                    Locations in India
+                    <i className="fas fa-map-marker-alt mr-2"></i> Locations in India
                   </NavLink>
                 </li>
 
                 {/* Media with Dropdown */}
                 <li className="relative">
                   <button onClick={toggleMediaMenu} className="flex pl-3 items-center text-white hover:text-[#f08619]">
-                    Media
+                    <i className="fas fa-film mr-2"></i> Media
                     <i className={`ml-1 fas ${isMediaOpen ? "fa-chevron-up" : "fa-chevron-down"}`}></i>
                   </button>
                   {isMediaOpen && (
                     <ul className="absolute top-full left-0 w-40 mt-2 bg-[#491c1d] text-white rounded shadow-lg">
                       <li>
-                        <NavLink to="/media" className="block px-4 py-2 hover:bg-[#f08619]">Media</NavLink>
+                        <NavLink to="/media" className="block px-4 py-2 hover:bg-[#f08619]">
+                          <i className="fas fa-play-circle mr-2"></i> Media
+                        </NavLink>
                       </li>
                       <li>
-                        <NavLink to="/gallery" className="block px-4 py-2 hover:bg-[#f08619]">Gallery</NavLink>
+                        <NavLink to="/gallery" className="block px-4 py-2 hover:bg-[#f08619]">
+                          <i className="fas fa-image mr-2"></i> Gallery
+                        </NavLink>
                       </li>
                       <li>
-                        <NavLink to="'/blogs'" className="block px-4 py-2 hover:bg-[#f08619]">Blogs</NavLink>
+                        <NavLink to="/blogs" className="block px-4 py-2 hover:bg-[#f08619]">
+                          <i className="fas fa-blog mr-2"></i> Blogs
+                        </NavLink>
                       </li>
                     </ul>
                   )}
@@ -136,11 +142,9 @@ const Navbar = () => {
                 {/* Other Nav Links */}
                 <li>
                   <NavLink to="/contact" className={({ isActive }) =>
-                      `block py-2 pl-3 pr-4 rounded ${
-                        isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"
-                      } lg:bg-transparent lg:p-0`
+                      `block py-2 pl-3 pr-4 rounded ${isActive ? "bg-[#f08619] text-white lg:text-[#f08619]" : "text-white hover:text-[#f08619]"} lg:bg-transparent lg:p-0`
                     }>
-                    Association & Support
+                    <i className="fas fa-phone mr-2"></i> Association & Support
                   </NavLink>
                 </li>
               </ul>
