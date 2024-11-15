52cdede8d47dcb67ce20473c225544cddb4a3978
sadre <mrsadre20@gmail.com>
2024-11-15 15:14:46 +0530

    text change

--- src/pages/Kumbh.jsx
+++ src/pages/Kumbh.jsx
@@ -3,25 +3,7 @@ import kumbh1 from "../kumbhmela/sample.jpg";
 import kumbhBg from '../kumbhmela/kumbh.mp4'
 
 const Kumbh = () => {
-  const [text, setText] = useState("Lorem ipsum dolor sit amet.");
 
-  useEffect(() => {
-    const texts = [
-      "Decrease body fat",
-      "Increase muscle strength",
-      "Improve flexibility",
-      "Enhance overall fitness",
-    ];
-
-    let index = 0;
-
-    const interval = setInterval(() => {
-      index = (index + 1) % texts.length;
-      setText(texts[index]);
-    }, 2000); // Change text every 2 seconds
-
-    return () => clearInterval(interval);
-  }, []);
 
 
   return (
@@ -50,7 +32,7 @@ const Kumbh = () => {
                 id="change"
                 className=" text-4xl sm:text-5xl mt-2 font-[Satisfy] text-white"
               >
-             {text}
+             Lorem ipsum dolor sit amet consectetur.
               </p>
             </div>
 
