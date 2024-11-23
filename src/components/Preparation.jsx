6086ce77a5032554067da150ffb2fbfebe76fbf9
sadre <mrsadre20@gmail.com>
2024-11-23 16:34:17 +0530

    some changes in home

--- src/components/Preparation.jsx
+++ src/components/Preparation.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import preparaton from '../kumbhmela/preparation.jpg'
+import React from "react";
+import preparaton from "../kumbhmela/preparation.jpg";
 
 const Preparation = () => {
   return (
     <>
-    <section id="preparation" className='py-16 bg-gradient-to-r from-orange-800 to-white'>
+      <section
+        id="preparation"
+        className="py-16 bg-gradient-to-r from-orange-800 to-white"
+      >
         <div className="flex justify-center flex-wrap-reverse lg:justify-around">
-      <div id="text-content" className="lg:w-[42%] w-[80%] hidden-element" >
-          <span className="text-base font-[Catamaran] font-light">Popular All Over World</span>
-          <h2 className="sm:text-6xl text-3xl font-bold  mt-8 font-[Catamaran]">Veda Pro Gives You An Extra Energy For Boost</h2>
-          <p className="sm:text-lg text-base mt-6 " >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-            dolore consectetur voluptatem, soluta dolorum aut nostrum neque,
-            quod dicta quaerat temporibus corrupti atque laudantium debitis
-            cumque ipsa adipisci autem assumenda?
-            dolore consectetur voluptatem, soluta dolorum aut nostrum neque,
-            quod dicta quaerat temporibus corrupti atque laudantium debitis
-            cumque ipsa adipisci autem assumenda?
-            dolore consectetur voluptatem, soluta dolorum aut nostrum neque,
-            quod dicta quaerat temporibus corrupti atque laudantium debitis
-            cumque ipsa adipisci autem assumenda?
-          </p>
+          <div id="text-content" className="lg:w-[42%] w-[80%] hidden-element">
+            <span className="text-base font-[Catamaran] font-light">
+              Preparations Of Maha Kumbh Mela 2025
+            </span>
+            <h2 className="sm:text-6xl text-3xl font-bold  mt-8 font-[Catamaran]">
+              Sanatana Dharma Foundation and Akhada Society proudly hoisted the
+              flag
+            </h2>
+            <p className="sm:text-lg text-base mt-6 ">
+              Today in Prayagraj, the Sanatana Dharma Foundation and Akhada
+              Society proudly raised the flag, marking the start of preparations
+              for the grand Mahakumbh Mela 2025. This sacred event symbolizes
+              unity, faith, and tradition, bringing people together to celebrate
+              spirituality and culture. The flag-hoisting ceremony is a
+              significant step toward this historic gathering
+            </p>
+          </div>
+          <img
+            src={preparaton}
+            loading="lazy"
+            className="lg:w-[50%] w-[80%]"
+            alt="sanatan dharma foundation maha kubh mela 2025 preparation"
+          />
         </div>
-       <img src={preparaton} loading='lazy' className="lg:w-[50%] w-[80%]" alt="sanatan dharma foundation maha kubh mela 2025 preparation" />
-        </div>
-       
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Preparation
+export default Preparation;
