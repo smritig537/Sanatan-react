103d4215f7e7af9a23ef3f765f94c2695a91f1af
sadre <mrsadre20@gmail.com>
2024-11-29 11:12:23 +0530

    pooja image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,40 @@ import Divider from "../components/Divider";
 import swamiji2 from "../assets/roll2.webp";
 import Banner from "../components/Banner";
 import guru from "../assets/guru.jpg";
-import guruji from "../assets/roll3.webp"
+import guruji from "../assets/roll3.webp";
 import HomeCard from "../components/HomeCard";
-import KumbhMelaSection from "../components/KumbhMelaSection"
+import KumbhMelaSection from "../components/KumbhMelaSection";
 import Preparation from "../components/Preparation";
 import PreparationImage from "../kumbhmela/preparation.jpg";
-import {Helmet} from 'react-helmet-async'
+import { Helmet } from "react-helmet-async";
+import PoojaImage from "Sacred and transformative Pooja.webp";
 
 const Home = () => {
   return (
     <>
-  <Helmet>
-  <title>Sanatana Dharma Foundation - Spiritual Workshops & Vedic Traditions</title>
-  <meta name="description" content="Sanatana Dharma Foundation offers spiritual workshops, Vedic traditions, and meditation retreats to help you reconnect with ancient wisdom and spiritual growth." />
-  <meta name="keywords" content="Sanatana Dharma Foundation, Hinduism, spiritual workshops, Vedic traditions, meditation retreats, cultural festivals, non-profit organization" />
-  <meta name="robots" content="index, follow" />
-  <meta property="og:title" content="Sanatana Dharma Foundation" />
-  <meta property="og:description" content="Join Sanatana Dharma Foundation for spiritual workshops, Vedic traditions, and meditation retreats to reconnect with ancient wisdom." />
-  <meta property="og:image" content={Swamiji} />
-  <link rel="canonical" href="https://sanatanadharmafoundation.co/" />
+      <Helmet>
+        <title>
+          Sanatana Dharma Foundation - Spiritual Workshops & Vedic Traditions
+        </title>
+        <meta
+          name="description"
+          content="Sanatana Dharma Foundation offers spiritual workshops, Vedic traditions, and meditation retreats to help you reconnect with ancient wisdom and spiritual growth."
+        />
+        <meta
+          name="keywords"
+          content="Sanatana Dharma Foundation, Hinduism, spiritual workshops, Vedic traditions, meditation retreats, cultural festivals, non-profit organization"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Sanatana Dharma Foundation" />
+        <meta
+          property="og:description"
+          content="Join Sanatana Dharma Foundation for spiritual workshops, Vedic traditions, and meditation retreats to reconnect with ancient wisdom."
+        />
+        <meta property="og:image" content={Swamiji} />
+        <link rel="canonical" href="https://sanatanadharmafoundation.co/" />
 
-  <script type="application/ld+json">
-    {`
+        <script type="application/ld+json">
+          {`
       {
         "@context": "https://schema.org",
         "@type": "Organization",
@@ -46,62 +58,85 @@ const Home = () => {
         ]
       }
     `}
-    </script>
-</Helmet>
-
-
+        </script>
+      </Helmet>
 
       <Banner />
-      <section id="header_section" className="w-full p-3 flex justify-center items-center flex-col">
-      <h1 className="text-[#491c1d] font-bold text-2xl w-[90%] sm:text-5xl text-center">
+      <section
+        id="header_section"
+        className="w-full p-3 flex justify-center items-center flex-col"
+      >
+        <h1 className="text-[#491c1d] font-bold text-2xl w-[90%] sm:text-5xl text-center">
           Sanatana Dharma Foundation
         </h1>
-        <p className="items-center w-[90%] mb-2 mt-3 text-justify rounded-sm"><i>
-          The Sanatana Dharma Foundation is dedicated to helping individuals
-          reconnect with the{" "}
-          <a
-            href="https://sanatanadharmafoundation.co/sanatanadharma"
-            className="text-blue-600 underline"
-          >
-            teachings of Sanatana Dharma
-          </a>{" "}
-          (Hinduism). It offers a range of programs, including spiritual
-          workshops, cultural festivals, and meditation retreats, designed to
-          provide guidance and support on one's spiritual journey. By preserving
-          Vedic traditions and promoting Hindu philosophy, the foundation
-          creates a vibrant community space for learning, growth, and
-          connection, fostering a deeper understanding of ancient wisdom and
-          values.
-          </i></p>
+        <p className="items-center w-[90%] mb-2 mt-3 text-justify rounded-sm">
+          <i>
+            The Sanatana Dharma Foundation is dedicated to helping individuals
+            reconnect with the{" "}
+            <a
+              href="https://sanatanadharmafoundation.co/sanatanadharma"
+              className="text-blue-600 underline"
+            >
+              teachings of Sanatana Dharma
+            </a>{" "}
+            (Hinduism). It offers a range of programs, including spiritual
+            workshops, cultural festivals, and meditation retreats, designed to
+            provide guidance and support on one's spiritual journey. By
+            preserving Vedic traditions and promoting Hindu philosophy, the
+            foundation creates a vibrant community space for learning, growth,
+            and connection, fostering a deeper understanding of ancient wisdom
+            and values.
+          </i>
+        </p>
       </section>
 
-      <Preparation  title="Preparations Of Maha Kumbh Mela 2025" 
-      heading='The Sanatana Dharma Foundation and Akhada Society 
-      Preparation Of Maha Kumbh Mela 2025' description="11-23-2024 in Prayagraj, 
+      <Preparation
+        title="Preparations Of Maha Kumbh Mela 2025"
+        heading="The Sanatana Dharma Foundation and Akhada Society 
+      Preparation Of Maha Kumbh Mela 2025"
+        description="11-23-2024 in Prayagraj, 
       the Sanatana Dharma Foundation and Akhada Society proudly raised the flag, 
       marking the start of preparations for the grand Mahakumbh Mela 2025. This 
       sacred event symbolizes unity, faith, and tradition, bringing people together
        to celebrate spirituality and culture. The flag-hoisting ceremony is a significant 
-       step toward this historic gathering" image={PreparationImage}/>
-      <KumbhMelaSection/>
+       step toward this historic gathering"
+        image={PreparationImage}
+      />
+      <KumbhMelaSection />
 
-      
+      <Preparation
+        heading="Sacred Union of Devotion and Spiritual Guidance"
+        description="Experience a transformative pooja ceremony graced by the divine presence of Shri Shri Narayanand Giri Maharaj, Chairman of the Sanatana Dharma Foundation, and Shri Madhu Pandit Dasa, President of ISKCON. This sacred gathering embodies the essence of devotion, unity, and spiritual enlightenment, inspiring profound connections to the timeless teachings of Sanatana Dharma."
+        image={PoojaImage}
+      />
+
       <section className="grid lg:grid-cols-3">
-      <HomeCard id="card" link="/about" heading="ABOUT THIS FOUNDATION" icon="fas fa-info-circle text-white"
-       text="In the bustling world we live in today, it can be easy to get swept away in the chaos and noise of everyday life. The demands of modern living often lead us to neglect our inner selves. It’s crucial to take a step back and reconnect with our spiritual roots, cultural heritage, and the ancient wisdom that has guided generations before us. This is where organizations like the Sanatana Dharma Foundation come in, providing a nurturing space for individuals to explore and delve into the profound teachings of Sanatana Dharma, also known as Hinduism. Through various programs and activities, the foundation fosters a sense of community and understanding, helping individuals rediscover the timeless values and practices that can enrich their lives and promote personal growth."/>
-     
-     <HomeCard id="card" link="/about" heading="OUR MISSION" icon="fa-solid fa-bullseye text-white"
-       text="Whether you seek spiritual guidance, philosophical teachings, or simply a community of like-minded individuals, the Sanatana Dharma Foundation provides a welcoming space for exploration and growth. Here, individuals can deepen their connection to the sacred texts and teachings of Hinduism, engaging in meaningful discussions and practices that resonate with their spiritual journeys. Through outreach programs and cultural events, the foundation is dedicated to spreading messages of peace, love, and unity to all who are open. If you're looking to reconnect with your spiritual roots, enhance your understanding of ancient wisdom, and connect with a vibrant community of seekers, consider getting involved with the Sanatana Dharma Foundation. Your transformative spiritual journey awaits!"/>
-     
-      <HomeCard id="card" link="/sanatanadharma" heading="CORE OF SANATANA DHARMA" icon="fa-brands fa-pagelines text-white"
-       text="At the core of Sanatana Dharma are timeless Sanskrit teachings passed down through generations. These teachings cover concepts like karma—the law of cause and effect—and the importance of upholding moral values in daily life. The Sanatana Dharma Foundation guides individuals on their spiritual journeys through yoga workshops, enlightenment, and interfaith initiatives. A key aspect is fostering community and connection. Through programs like education classes, cultural festivals, and meditation retreats, the foundation creates a space for individuals to learn from one another. By preserving Vedic traditions and promoting Hindu philosophy, the foundation ensures the spiritual heritage of Sanatana Dharma continues to thrive."/>
-     
-     
-     </section>
-     
+        <HomeCard
+          id="card"
+          link="/about"
+          heading="ABOUT THIS FOUNDATION"
+          icon="fas fa-info-circle text-white"
+          text="In the bustling world we live in today, it can be easy to get swept away in the chaos and noise of everyday life. The demands of modern living often lead us to neglect our inner selves. It’s crucial to take a step back and reconnect with our spiritual roots, cultural heritage, and the ancient wisdom that has guided generations before us. This is where organizations like the Sanatana Dharma Foundation come in, providing a nurturing space for individuals to explore and delve into the profound teachings of Sanatana Dharma, also known as Hinduism. Through various programs and activities, the foundation fosters a sense of community and understanding, helping individuals rediscover the timeless values and practices that can enrich their lives and promote personal growth."
+        />
+
+        <HomeCard
+          id="card"
+          link="/about"
+          heading="OUR MISSION"
+          icon="fa-solid fa-bullseye text-white"
+          text="Whether you seek spiritual guidance, philosophical teachings, or simply a community of like-minded individuals, the Sanatana Dharma Foundation provides a welcoming space for exploration and growth. Here, individuals can deepen their connection to the sacred texts and teachings of Hinduism, engaging in meaningful discussions and practices that resonate with their spiritual journeys. Through outreach programs and cultural events, the foundation is dedicated to spreading messages of peace, love, and unity to all who are open. If you're looking to reconnect with your spiritual roots, enhance your understanding of ancient wisdom, and connect with a vibrant community of seekers, consider getting involved with the Sanatana Dharma Foundation. Your transformative spiritual journey awaits!"
+        />
+
+        <HomeCard
+          id="card"
+          link="/sanatanadharma"
+          heading="CORE OF SANATANA DHARMA"
+          icon="fa-brands fa-pagelines text-white"
+          text="At the core of Sanatana Dharma are timeless Sanskrit teachings passed down through generations. These teachings cover concepts like karma—the law of cause and effect—and the importance of upholding moral values in daily life. The Sanatana Dharma Foundation guides individuals on their spiritual journeys through yoga workshops, enlightenment, and interfaith initiatives. A key aspect is fostering community and connection. Through programs like education classes, cultural festivals, and meditation retreats, the foundation creates a space for individuals to learn from one another. By preserving Vedic traditions and promoting Hindu philosophy, the foundation ensures the spiritual heritage of Sanatana Dharma continues to thrive."
+        />
+      </section>
+
       <main className="m-auto max-w-full w-[90%] lg:w-[986px] text-base sm:text-xl mb-10 p-5 sm:p-0 overflow-x-hidden">
-
-
         <img
           className="h-auto sm:h-[500px] object-cover object-center m-auto mb-5"
           src={Swamiji}
@@ -121,13 +156,16 @@ const Home = () => {
         </h2>
 
         <p className="text-gray-700 sm:text-justify text-left">
-          <strong>Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit Swami Narayan Nand Giriji Maharaj</strong> pursued multiple
-          careers before dedicating himself to spiritual leadership. He began as
-          a successful businessman, contributing to the economic landscape.
-          However, driven by his spiritual calling, he transitioned to become a
-          renowned spiritual leader. Additionally, he has been involved in the
-          fields of education and social activism, striving to create a positive
-          impact on society.
+          <strong>
+            Mahamadaleshwar Shri Shri 1008 Anant Shri Vibhushit Swami Narayan
+            Nand Giriji Maharaj
+          </strong>{" "}
+          pursued multiple careers before dedicating himself to spiritual
+          leadership. He began as a successful businessman, contributing to the
+          economic landscape. However, driven by his spiritual calling, he
+          transitioned to become a renowned spiritual leader. Additionally, he
+          has been involved in the fields of education and social activism,
+          striving to create a positive impact on society.
         </p>
 
         <h2 className="sm:text-4xl text-2xl mt-5 font-medium text-left sm:text-center text-orange-900">
