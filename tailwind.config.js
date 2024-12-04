b322797a348159507b4c2b857e19a0c1876dc982
sadre <mrsadre20@gmail.com>
2024-12-04 12:02:54 +0530

    animations add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,28 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(100%)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+      },
+      animation: {
+        'slide-up': 'slideUp 1.5s ease-out',
+      },
       fontFamily: {
         mukta: ["Mukta", "sans-serif"],
         oswald: ["Oswald", "sans-serif"],
       },
-      backgroundImage : {
-        'om-pattern' : "url('/src/assets/om.png')"
-      }
+      backgroundImage: {
+        'om-pattern': "url('/src/assets/om.png')",
+      },
     },
   },
   plugins: [],
